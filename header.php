9c3bfacde6fb115c5177251b30f52d77e06ed2e2
04rafdas <103262305+04rafdas@users.noreply.github.com>
2022-12-06 10:41:17 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LoginPage</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/reset.css">
+</head>
+<body>
+
+    <nav>
+        <div class="wrapper">
+            <a href="index.php"><img src="img/logo.png" alt="webbsidalogo"></a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="discover.php">About Us</a></li>
+                <li><a href="blog.php">About Blogs</a></li>
+                <li><a href="signup.php">Sign Up</a></li>
+                <li><a href="login.php">Log In</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="wrapper">
